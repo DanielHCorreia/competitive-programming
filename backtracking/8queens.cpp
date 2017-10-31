897acf0188a1a23b556dde10c19db34606a1d69b
SenhorN <danielgrupomh@gmail.com>
2017-10-31 15:46:49 -0300

    Update 8queens.cpp
    Arrumado o erro de apresentação

--- backtracking/8queens.cpp
+++ backtracking/8queens.cpp
@@ -12,7 +12,7 @@ bool avblPlace(int c, int r) {
 
 void backtrack(int c){
 	if (c == 8 && row[b] == a) {
-		printf("%2d		%d", ++lineCounter, row[0] + 1);
+		printf("%2d      %d", ++lineCounter, row[0] + 1);
 		for (int i = 1; i < 8; i++) printf(" %d", row[i] + 1);
 		printf("\n"); }
 	for (int r = 0; r < 8; r++)
@@ -21,12 +21,18 @@ void backtrack(int c){
 }
 
 int main() {
-    scanf("%d", &tc);
-    while (tc--) {
+    int T;
+    scanf("%d",&T);
+
+    for(int tc=0;tc<T;tc++){
         scanf("%d %d", &a, &b); a--; b--;
         memset(row, 0, sizeof(row)); lineCounter = 0;
+
+        if(tc!=0) printf("\n");
         printf("SOLN       COLUMN\n");
-		printf(" #      1 2 3 4 5 6 7 8\n\n");
-		backtrack(0);
-		if (tc) printf("\n"); }
-    return 0; }
+        printf(" #      1 2 3 4 5 6 7 8\n\n");
+        backtrack(0);
+
+      }
+    return 0;
+}
